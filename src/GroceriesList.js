baefc132eb65607c2e49341325f8f0992a7387f6
luca pomer <ucapomer@gmail.com>
2020-03-22 20:32:16 +0100

    trying to create dynamic list for product
    still need button delegation

--- src/GroceriesList.js
+++ src/GroceriesList.js
@@ -5,22 +5,29 @@ class GroceriesList extends React.Component{
 constructor(props){
     super(props);
     this.state={
-        listItems: [],
+        listItems: ["tomato ","pasta"],
     }
+    this.addProduct("basilikum");
+}
+addProduct(name){
+    let newList = this.state.listItems;
+    newList.concat(name);
+    this.setState({
+        listItems: newList
+    })
 }
 
     render(){
-        const products = ["tomato","pasta"];
-        
-        const listItems = products.map((product) =>
+        const listItemsCopy = this.state.listItems.map((product) =>
             <Product name={product}></Product>
         );
+
         return(
             <div>
                 <h1>Grocery List</h1>
                 <ul id={"productList"}>
                     <Product name={"test name"}/>
-                    {listItems}
+                    {this.state.listItems}
                 </ul>
             </div>
         )

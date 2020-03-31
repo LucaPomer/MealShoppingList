80f99e4f81ed9edf845ad6fe135750e00f4a5277
luca pomer <ucapomer@gmail.com>
2020-03-31 19:00:34 +0200

    you can change amount of product via buttons

--- src/GroceriesList.js
+++ src/GroceriesList.js
@@ -18,12 +18,11 @@ class GroceriesList extends React.Component {
         let mapCopy = this.state.productToAmount;
         for (let name of names) {
             let amount;
-            if(mapCopy.hasOwnProperty(name)){
+            if (mapCopy.hasOwnProperty(name)) {
                 amount = mapCopy[name.toString()];
                 amount++;
-            }
-            else{
-            amount=1;
+            } else {
+                amount = 1;
             }
             mapCopy[name.toString()] = amount;
         }
@@ -33,15 +32,35 @@ class GroceriesList extends React.Component {
         });
     }
 
-    removeProduct(name){
+    removeProductAmount(name) {
         let mapCopy = this.state.productToAmount;
-        if(mapCopy.hasOwnProperty(name)){
-            delete mapCopy[name.toString()];
+        let amount;
+        if (mapCopy.hasOwnProperty(name)) {
+            amount = parseInt(mapCopy[name.toString()]);
+            if(amount<=1){
+                delete mapCopy[name.toString()];
+            }
+            else{
+                amount--;
+                mapCopy[name.toString()] = amount;
+            }
         }
         this.setState({
             productToAmount: mapCopy,
         });
+    }
 
+    addProductAmount(name) {
+        let mapCopy = this.state.productToAmount;
+        let amount;
+        if (mapCopy.hasOwnProperty(name)) {
+            amount = parseInt(mapCopy[name.toString()]);
+                amount++;
+                mapCopy[name.toString()] = amount;
+        }
+        this.setState({
+            productToAmount: mapCopy,
+        });
     }
 
     render() {
@@ -49,7 +68,12 @@ class GroceriesList extends React.Component {
         const listWithDuplicatesCopy = this.state.productToAmount;
         let keys = Object.keys(listWithDuplicatesCopy);
         for (let key of keys) {
-            listOfProducts.push(<Product handleClickProduct={()=>this.removeProduct(key)} key={key} name={key} amount={listWithDuplicatesCopy[key]}/>)
+            listOfProducts.push(<Product
+                clickRemoveProduct={() => this.removeProductAmount(key)}
+                clickAddProduct={() => this.addProductAmount(key)}
+                key={key}
+                name={key}
+                amount={listWithDuplicatesCopy[key]}/>)
         }
         return (
             <div className={"groceryList"}>
@@ -64,13 +88,16 @@ class GroceriesList extends React.Component {
 }
 
 function Product(props) {
-    return <Container  fluid="md">
-        <Row >
-            <Col xs className={"productItem"}>
-                <ListGroupItem  variant={"secondary"}>{props.amount} x {props.name}</ListGroupItem>
+    return <Container fluid="md">
+        <Row className={"productItem"}>
+            <Col xs id={"description"}>
+                <ListGroupItem variant={"secondary"}>{props.amount} x {props.name}</ListGroupItem>
             </Col>
-            <Col xs={"auto"}>
-                <Button variant={"danger"} onClick={props.handleClickProduct}>remove</Button>
+            <Col id={"removeButton"} xs={"auto"}>
+                <Button variant={"danger"} onClick={props.clickRemoveProduct}>-</Button>
+            </Col>
+            <Col id={"addButton"} xs={"auto"}>
+                <Button variant={"success"} onClick={props.clickAddProduct}>+</Button>
             </Col>
         </Row>
     </Container>

f73806ad69ad583ad7753dffccc74dfe5ec2221e
luca pomer <ucapomer@gmail.com>
2020-04-04 15:35:27 +0200

    Updates

---
+++ precache-manifest.2741bd02187c9929dd448d8410511943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa75e5e58123ae133af64e98dc47da6",
+    "url": "/MealShoppingList/index.html"
+  },
+  {
+    "revision": "ccb1d447dbfe4f3e1daf",
+    "url": "/MealShoppingList/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d4c552185e01b1146fb2",
+    "url": "/MealShoppingList/static/css/main.fe6e3127.chunk.css"
+  },
+  {
+    "revision": "ccb1d447dbfe4f3e1daf",
+    "url": "/MealShoppingList/static/js/2.14b80a19.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MealShoppingList/static/js/2.14b80a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c552185e01b1146fb2",
+    "url": "/MealShoppingList/static/js/main.c0529579.chunk.js"
+  },
+  {
+    "revision": "e7079c5bf4d19a20f79a",
+    "url": "/MealShoppingList/static/js/runtime-main.8bb06ebe.js"
+  }
+]);
